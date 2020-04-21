48e178b535894cf5bf378bd2e1e2142067c2cab1
nazzzario <nazzzario23@gmail.com>
2020-04-21 12:30:27 +0300

    Add Freemarker template

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -25,9 +25,16 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String,Object> model){
+    public String main(@RequestParam(required = false,defaultValue = "") String filter,
+                       Model model){
         Iterable<Message> messages = messageRepos.findAll();
-        model.put("massages",messages);
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepos.findByTag(filter);
+        }else {
+            messages = messageRepos.findAll();
+        }
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter",filter);
         return "main";
     }
     @PostMapping("/main")
@@ -40,18 +47,6 @@ public class MainController {
         Iterable<Message> messages = messageRepos.findAll();
         model.put("messages",messages);
 
-        return "main";
-    }
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,
-                         Map<String,Object> model){
-        Iterable<Message> messages;
-        if(filter != null && !filter.isEmpty()) {
-            messages = messageRepos.findByTag(filter);
-        }else {
-           messages = messageRepos.findAll();
-        }
-        model.put("messages", messages);
         return "main";
     }
 }
