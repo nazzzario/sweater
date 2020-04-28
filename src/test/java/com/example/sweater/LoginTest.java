58809a71a73f8ca0cb3629f9cfe43773e90912dd
Nazar Krasnovoronka <nazzzario23@gmail.com>
2020-04-28 17:53:37 +0300

    add test cases

---
+++ src/test/java/com/example/sweater/LoginTest.java
@@ -0,0 +1,61 @@
+package com.example.sweater;
+
+import com.example.sweater.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+    @Autowired
+    private MainController mainController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(mainController).isNotNull();
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello user")))
+                .andExpect(content().string(containsString("Please, login")));
+    }
+
+    @Test
+    public void loginTest() throws Exception{
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+
+    }
+
+    @Test
+    public void correctLoginTest() throws Exception{
+        this.mockMvc.perform(formLogin().user("user").password("1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void badCredential() throws Exception{
+        this.mockMvc.perform(post("/login").param("don","2"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+
+    }
+}
---
+++ src/test/java/com/example/sweater/MainControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.sweater;
+
+import com.example.sweater.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MainController mainController;
+
+    @Test
+    public void mainPageTest() throws Exception{
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+    }
+}
